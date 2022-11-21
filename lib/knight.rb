30aace3b1077f09d230a9f7780b9bfac2b5e3be6
Joseph Petersen <joseph.j.petersen@gmail.com>
2022-11-21 15:59:08 -0500

    Write knight class

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 # Class defining the Knight piece
-
 class Knight
   attr_accessor :position, :children, :parent
 
-  def initialize
+  MOVES = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]].freeze
+
+  def initialize(position, parent = nil)
+    @position = position
+    @children = []
+    @parent = parent
   end
 
-  
+  def next_moves
+    next_moves = MOVES.map do |move|
+      move.each_with_index.map { |n, i| n + @position[i] unless (n + @position[i].negative? || (n + @position[i]) > 7) }
+    end
+    next_moves.delete_if { |move| move.include?(nil)}
+  end
+end
